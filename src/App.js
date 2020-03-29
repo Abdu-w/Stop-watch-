187dd29ca8a27a406993f3373ea646ce5d9503e6
Student_MA109 <student_ma109@Student-MA109.lan1>
2020-03-28 22:27:13 -0400

    update start

--- src/App.js
+++ src/App.js
@@ -9,44 +9,53 @@ import './App.css';
 //if click then set count to 0
 export default class  App extends Component {
   constructor(props){
-    super(props) // have to if using constructor 
+    super(props) // have to if using constructor // js!
     // set a state obj with count == 0
     this.state = {
       count : 0
     }
-     // need a null position to pause 
-     this.counter = null
+    // need a null position to pause 
+    this.counter = null
   }
-   // onclick set the counter to the current number 
-    // increase the count by 1 in 1000 ms
-    startCount = () => {
-      this.counter = setInterval(()=>{
-        this.setState({count: this.state.count + 1})  
-      }, 1000)
-    }
 
-    // on click it will hold the setInterval 
-    // should not start before start 
-    pauseCount = () => {
-      clearInterval(this.counter);
-    } 
-    // set counter to 0 
-    resetCount = () => {
-      clearInterval(this.counter);
-      this.setState({ count: 0 })
+  // onclick set the counter to the current number 
+  // increase the count by 1 in 1000 ms
+  startCount = () => {
+    this.counter = setInterval(()=>{
+      this.setState({count: this.state.count + 1})  
+    }, 1000)
+  }
+
+  // on click it will hold the setInterval 
+  // should not start before start 
+  pauseCount = () => {
+    clearInterval(this.counter);
   }
   
-   
+  // set counter to 0 
+  resetCount = () => {
+    clearInterval(this.counter);
+    this.setState({ count: 0 })
+  }
+
+
   render(){
     return (
       <div className="App">
-         <h1>{this.state.count}</h1><br></br>
-          <button className="rest" onClick={this.resetCount} >Reset</button>
-          <button className="start" onClick={this.startCount}>Start</button>
-          <button className="pause"onClick={this.pauseCount} >Pause</button>
-
-      </div>
         
+        <h1>{this.state.count}</h1><br></br>
+        <button className="rest" onClick={this.resetCount}>Reset</button>
+        <button className="start" onClick={this.startCount}>
+          {
+            this.state.count === 0
+            // just etxra make start button stay continue 
+            ? "start"
+            : "continue"
+           
+          }
+        </button>
+        <button className="pause" onClick={this.pauseCount}>Pause</button>
+      </div>
   
       );
     
@@ -54,6 +63,7 @@ export default class  App extends Component {
   
  
 }
+
 
 // souces:
 // https://www.youtube.com/watch?v=NAx76xx40jM
