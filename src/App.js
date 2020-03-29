4c1d9c4f9ab222e9577a64947f8a93c6af51d1ff
Student_MA109 <student_ma109@Student-MA109.lan1>
2020-03-28 22:18:42 -0400

    pause state

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ export default class  App extends Component {
          <h1>{this.state.count}</h1><br></br>
           <button className="rest" >Reset</button>
           <button className="start" onClick={this.startCount}>Start</button>
-          <button className="pause" >Pause</button>
+          <button className="pause"onClick={this.pauseCount} >Pause</button>
 
       </div>
         
